2233d7d91e36f7f8f1836e555dc50643f9a300be
Daniel Garcia <github@mail.daniel.es>
2025-05-01 11:36:32 +0000

    Update supabase client

--- src/components/authentication/signin.tsx
+++ src/components/authentication/signin.tsx
@@ -1,11 +1,5 @@
 import { useState, useEffect } from 'react';
-import { createClient } from '@supabase/supabase-js';
-
-// Create Supabase client
-const supabase = createClient(
-  'https://mfbsesgwswgtlqnqqaap.supabase.co',
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mYnNlc2d3c3dndGxxbnFxYWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwMzYzNjgsImV4cCI6MjA2MTYxMjM2OH0.XzU2OPfCF5bHNWFhavzzhXFYD8v2AQTI8oK763huUVQ'
-);
+import supabase from '~/lib/supabase';
 
 type SignInState = 'EMAIL_INPUT' | 'OTP_INPUT' | 'SUBMITTING' | 'ERROR';
 
--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,8 @@
-import { createClient } from "@supabase/supabase-js";
+import { createClient } from '@supabase/supabase-js';
 
-export const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY
+const supabase = createClient(
+  'https://mfbsesgwswgtlqnqqaap.supabase.co',
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1mYnNlc2d3c3dndGxxbnFxYWFwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDYwMzYzNjgsImV4cCI6MjA2MTYxMjM2OH0.XzU2OPfCF5bHNWFhavzzhXFYD8v2AQTI8oK763huUVQ'
 );
+
+export default supabase;
