ef540f5acf8a59cbd2b13f1ebf00074fc497f205
Daniel Garcia <github@mail.daniel.es>
2025-05-07 10:02:44 +0000

    Add single session management for users
    - Implemented a signal to terminate other active sessions upon user login if SINGLE_SESSION_PER_USER setting is enabled.
    - Added SINGLE_SESSION_PER_USER setting in core settings to control session behavior.

--- backend/authentication/signals.py
+++ backend/authentication/signals.py
@@ -1,10 +1,21 @@
-"""
-singnals.py
+from django.contrib.sessions.models import Session
+from django.contrib.auth.signals import user_logged_in
+from django.utils import timezone
+from django.conf import settings
 
-This file contains the signals for the authentication app.
-"""
 
-import logging
+def terminate_other_sessions(sender, user, request, **kwargs):
+    # Only terminate other sessions if SINGLE_SESSION_PER_USER setting is enabled
+    if getattr(settings, "SINGLE_SESSION_PER_USER", True):
+        current_session_key = request.session.session_key
+        sessions = Session.objects.filter(expire_date__gte=timezone.now())
+        for session in sessions:
+            data = session.get_decoded()
+            if (
+                data.get("_auth_user_id") == str(user.id)
+                and session.session_key != current_session_key
+            ):
+                session.delete()
 
-log = logging.getLogger(__name__)
 
+user_logged_in.connect(terminate_other_sessions)
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -361,6 +361,9 @@ SPECTACULAR_SETTINGS = {
 
 AUTH_USER_MODEL = "authentication.User"
 
+# Single session per user
+SINGLE_SESSION_PER_USER = True  # Set to False to allow multiple sessions per user
+
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
